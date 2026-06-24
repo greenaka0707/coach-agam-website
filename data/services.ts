fb9950f3f0adc7d3d79107b62ba1939a323ad386
greenaka0707 <produkpebeka@gmail.com>
2026-06-24 23:23:07 +0700

    Create services.ts

---
+++ data/services.ts
@@ -0,0 +1,18 @@
+export const services = [
+  {
+    title: "Workshop & Seminar",
+    slug: "workshop-seminar",
+  },
+  {
+    title: "Coach Mentoring",
+    slug: "coach-mentoring",
+  },
+  {
+    title: "Football Consulting",
+    slug: "football-consulting",
+  },
+  {
+    title: "Sports Science Services",
+    slug: "sports-science",
+  },
+];
