d7240a37ca73353c66e005ee95f589f5bdb4cfcc
greenaka0707 <produkpebeka@gmail.com>
2026-06-24 23:16:07 +0700

    Add site configuration for Coach Agam

---
+++ data/site.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  name: "Coach Agam",
+  title: "Football Educator",
+  description:
+    "Football Educator, Sports Science Practitioner, and Football Development Consultant.",
+  url: "https://coachagam.com",
+};
