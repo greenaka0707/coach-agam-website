9a4ed6c873c67a2b7867f07c9ad949e6f9cb56f5
greenaka0707 <produkpebeka@gmail.com>
2026-06-24 23:22:28 +0700

    Create navigation.ts

---
+++ data/navigation.ts
@@ -0,0 +1,30 @@
+export const navigation = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Expertise",
+    href: "/expertise",
+  },
+  {
+    title: "Services",
+    href: "/services",
+  },
+  {
+    title: "Resources",
+    href: "/resources",
+  },
+  {
+    title: "Portfolio",
+    href: "/portfolio",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+];
