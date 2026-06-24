97228ae83c5c4bf96ed88cb0f83feb348b156b71
greenaka0707 <produkpebeka@gmail.com>
2026-06-24 23:23:39 +0700

    Create expertise.ts

---
+++ data/expertise.ts
@@ -0,0 +1,14 @@
+export const expertise = [
+  {
+    title: "Coach Education",
+  },
+  {
+    title: "Sports Science",
+  },
+  {
+    title: "Football Methodology",
+  },
+  {
+    title: "Football Development",
+  },
+];
