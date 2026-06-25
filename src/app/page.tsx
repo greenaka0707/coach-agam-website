e3810694f1e3cb45d4a0fb28fe0d623abd83648d
greenaka0707 <produkpebeka@gmail.com>
2026-06-25 09:14:22 +0700

    Create page.tsx
    feat: create homepage

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Coach Agam</h1>
+
+      <p>
+        Football Educator • Sports Science Practitioner • Football Development
+        Consultant
+      </p>
+    </main>
+  );
+}
